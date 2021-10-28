2082f6890132a35740cebfe58617e1a6ce0a4b90
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2021-10-28 12:48:11 +0200

    add documentation

--- js/check-active.js
+++ js/check-active.js
@@ -1,7 +1,7 @@
-const liens = document.querySelectorAll('#navbar a')
+const liens = document.querySelectorAll('#navbar a') //on selectionne tous les liens de la barre de navigation
 
-liens.forEach((lien) => {
-    if (lien.pathname === window.location.pathname) {
-       lien.parentElement.classList.add('active');
+liens.forEach((lien) => { //pour chaque lien recupere
+    if (lien.pathname === window.location.pathname) { //si le chemin du lien correspond au chemin de la page actuelle
+       lien.parentElement.classList.add('active'); //on ajoute la classe active au conteneur du lien
     }
 })
--- js/valid-birthdate.js
+++ js/valid-birthdate.js
@@ -1,8 +1,9 @@
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth() + 1; //January is 0!
+let today = new Date(); //on cree un date a la date d'aujourd'hui
+let dd = today.getDate(); //on recupere le jour
+let mm = today.getMonth() + 1; //on recupere le mois. Attention, janvier correspond au mois 0 !
 let yyyy = today.getFullYear() - 12; //la personne doit avoir au moins 12 ans
 
+//si le jour ou le mois est inferieur a 10, on rajoute un 0 devant
 if (dd < 10) {
    dd = '0' + dd;
 }
@@ -12,4 +13,4 @@ if (mm < 10) {
 } 
     
 today = yyyy + '-' + mm + '-' + dd;
-document.getElementById('birthdate').setAttribute('max', today);
+document.getElementById('birthdate').setAttribute('max', today); // on met la date maximale de l'input birthdate a la date definie dans today (aujourd'hui - 12 annees)
