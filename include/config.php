2328738e8f8fd7e0c9bc44c41b86a9638222d70b
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2021-11-09 15:17:45 +0100

    navbar avec lien active

--- include/config.php
+++ include/config.php
@@ -3,3 +3,8 @@ const TEL_NUM = "641850305";
 const MAIL_ADDRESS = "guilhem.richaud@etu.univ-lyon1.fr";
 const POST_ADDRESS = "3 Avenue Pierre Semard, 01000 Bourg-en-Bresse";
 
+function addActive($link) {
+    if ($_SERVER['PHP_SELF'] == $link) {
+        echo 'class=\'active\'';
+    } 
+}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-/*ADD ACTIVE CLASS TO CURRENT PAGE LINK*/
-
-const liens = document.querySelectorAll('#navbar a') //on selectionne tous les liens de la barre de navigation
-
-liens.forEach((lien) => { //pour chaque lien recupere
-    if (lien.pathname === window.location.pathname) { //si le chemin du lien correspond au chemin de la page actuelle
-       lien.parentElement.classList.add('active'); //on ajoute la classe active au conteneur du lien
-    }
-})
-
 /*VALIDATE MAIL*/
 
 // on defini des constantes qui representent des parties de notre page
